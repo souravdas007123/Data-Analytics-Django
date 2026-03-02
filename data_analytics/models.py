c0a2677562595b96a0248a85f56299e76f411f36
Sourav Das <souravdas007123@gmail.com>
2026-03-02 11:44:44 +0530

    update

--- data_analytics/models.py
+++ data_analytics/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Data(models.Model):
     first_name=models.CharField(max_length=100)
     last_name=models.CharField(max_length=100)
-    age=models.PositiveIntegerField(max_length=2)
+    age=models.IntegerField()
     salary=models.PositiveIntegerField()
